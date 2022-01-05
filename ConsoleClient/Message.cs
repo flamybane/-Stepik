66b763d7a9558781a657aad1cf6036674a64d8f7
Stoulz Padre <stoper6811@mail.ru>
2022-01-05 22:13:16 +0300

    !

--- ConsoleClient/Message.cs
+++ ConsoleClient/Message.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace _Stepik
 {
+    [Serializable]
     public class Message
     {
 
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace _Stepik
 {
@@ -6,9 +7,13 @@ namespace _Stepik
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Начало проекта!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //Console.WriteLine("Начало проекта!");
+            //Console.WriteLine(msg.ToString());
         }
     }
 }
