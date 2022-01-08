043696b9ba0eb5327c25b556ac2fa92bc26ea4a3
Stoulz Padre <stoper6811@mail.ru>
2022-01-09 01:26:12 +0300

    КАЙФЫ!

--- ConsoleClient/MessengerClientAPI.cs
+++ ConsoleClient/MessengerClientAPI.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +25,7 @@ namespace _Stepik
 
         public Message GetMessage(int MessageId)
         {
-            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger/1");
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger/" + MessageId.ToString());
             request.Method = "Get";
             WebResponse response = request.GetResponse();
             string status = ((HttpWebResponse)response).StatusDescription;
@@ -35,13 +37,33 @@ namespace _Stepik
             reader.Close();
             dataStream.Close();
             response.Close();
-            if((status.ToLower() == "ok") && (responseFromServer != "Not found"))
+            if ((status.ToLower() == "ok") && (responseFromServer != "Not found"))
             {
-                Message deserializedMsg = JsonConvert.DeserializeObject<Message>(response);
+                Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responseFromServer);
                 //Console.WriteLine(deserializedMsg);
                 return deserializedMsg;
             }
             return null;
+        }
+        public bool SendMessage(Message msg)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger");
+            request.Method = "POST";
+            string postData = JsonConvert.SerializeObject(msg);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return true;
         }
     }
 }
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -5,15 +5,48 @@ namespace _Stepik
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+
+        private static void GetMessage()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
         static void Main(string[] args)
         {
-            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(deserializedMsg);
+            //Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+            //Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deserializedMsg);
             //Console.WriteLine("Начало проекта!");
             //Console.WriteLine(msg.ToString());
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя:");
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }    
+        }
+
+        private static void GetNewMessages()
+        {
+            throw new NotImplementedException();
         }
     }
 }
